ab7d92d52e2818b1b34efbae39f931b092ac5614
ambivvy <117640809+floorceiling@users.noreply.github.com>
2023-04-19 18:11:49 +0100

    added scripts

--- SplitSensesGroup6/Assets/scripts/spike.cs
+++ SplitSensesGroup6/Assets/scripts/spike.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bullet : MonoBehaviour
+public class spike : MonoBehaviour
 {
-
- 
     // Start is called before the first frame update
+
 
     // Update is called once per frame
 
-    
-
+//private void OnCollisionEnter3D(Collision GameObject)
+//    {
+//        Destroy;
+//    }
+//
+//
 }
